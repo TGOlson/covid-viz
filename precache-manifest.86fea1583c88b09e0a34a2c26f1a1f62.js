723cc035ec909f9102b9c8a861b8fd5eaa6b3e8c
Tyler Olson <tydotg@gmail.com>
2020-04-04 18:49:37 +0800

    Updates

---
+++ precache-manifest.86fea1583c88b09e0a34a2c26f1a1f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14808fe7ed2847014030d2c939630b1c",
+    "url": "/covid-viz/index.html"
+  },
+  {
+    "revision": "41b2a5ad0b3dab72061a",
+    "url": "/covid-viz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "40702349429f7be25cec",
+    "url": "/covid-viz/static/js/2.e0b02088.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-viz/static/js/2.e0b02088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b2a5ad0b3dab72061a",
+    "url": "/covid-viz/static/js/main.a2802318.chunk.js"
+  },
+  {
+    "revision": "b29f8a455b8c6460193c",
+    "url": "/covid-viz/static/js/runtime-main.fde3b118.js"
+  }
+]);
