b7b5902dfc5b45d0ba6808ee81a82dfb3d312184
Tyler Olson <tydotg@gmail.com>
2020-04-14 20:54:55 +0800

    Updates

---
+++ precache-manifest.7550491b03d4cb0b15abcc19dd6afada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce070c27dd54fb4d7578a29230db2d33",
+    "url": "/covid-viz/index.html"
+  },
+  {
+    "revision": "32cec159f7965d320c35",
+    "url": "/covid-viz/static/css/main.03b2f61d.chunk.css"
+  },
+  {
+    "revision": "0f6d99f862e990d005c2",
+    "url": "/covid-viz/static/js/2.b3ba44d2.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/covid-viz/static/js/2.b3ba44d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cec159f7965d320c35",
+    "url": "/covid-viz/static/js/main.82e9d59f.chunk.js"
+  },
+  {
+    "revision": "b29f8a455b8c6460193c",
+    "url": "/covid-viz/static/js/runtime-main.fde3b118.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-viz/precache-manifest.0228a15104e1ea74b29214e420c5a021.js"
+  "/covid-viz/precache-manifest.7550491b03d4cb0b15abcc19dd6afada.js"
 );
 
 self.addEventListener('message', (event) => {
